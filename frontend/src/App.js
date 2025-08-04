8d281a2735217b0152ed14bcecc4c7b41db6b8dc
souma <souma3789522@gmail.com>
2025-08-04 18:45:21 +0900

    feat(setup) complete connection frontend to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("loading...");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/hello")
+      .then(res => res.text())
+      .then(data => setMessage(data))
+      .catch(err => {
+        console.error("API error:", err);
+        setMessage("Error connecting to backend");
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Spring Boot からのメッセージ:</h1>
+      <p>{message}</p>
     </div>
   );
 }
