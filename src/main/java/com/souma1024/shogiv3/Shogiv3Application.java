805769755d70c834fbda15bd9505f884b07f0422
souma <souma3789522@gmail.com>
2025-08-04 16:52:44 +0900

    Initial commit

---
+++ src/main/java/com/souma1024/shogiv3/Shogiv3Application.java
@@ -0,0 +1,13 @@
+package com.souma1024.shogiv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shogiv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shogiv3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/souma1024/shogiv3/Shogiv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souma1024.shogiv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shogiv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
